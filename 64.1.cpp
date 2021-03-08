1beeb29b21f705f9339f3ff4cb26a793faad6b29
KlaudiaNarekiewicz <62132418+KlaudiaNarekiewicz@users.noreply.github.com>
2021-03-08 09:46:52 +0100

    Update 64.1.cpp

--- 64.1.cpp
+++ 64.1.cpp
@@ -5,15 +5,16 @@ using namespace std;
 const int SIZE = 200;
 ifstream in("dane_obrazki.txt");
 char tab[21][21];
-void zczytaj(){
+void zczytaj()
+{
   	for (int i=0; i<21; i++)
 	{
-    	for(int j=0; j<21; j++)
+    		for(int j=0; j<21; j++)
 		{
-      		if (i==20 && j==20) 
-			  return;
-      		in >> tab[i][j];
-    	}
+      			if (i==20 && j==20) 
+			return;
+      			in >> tab[i][j];
+    		}
   	}
 }
 
@@ -22,11 +23,11 @@ bool rewers()
   	int licznik_jedynek=0, licznik_zer=0;
   	for (int i=0; i<20; i++) 
 	{
-    	for (int j=0; j<20; j++) 
+    		for (int j=0; j<20; j++) 
 		{
-      		if (tab[i][j] == '0') licznik_zer++;
-      		if (tab[i][j] == '1') licznik_jedynek++;
-    	}
+      			if (tab[i][j] == '0') licznik_zer++;
+      			if (tab[i][j] == '1') licznik_jedynek++;
+    		}
   	}
   	return licznik_jedynek>licznik_zer;
 }
@@ -35,11 +36,11 @@ int razem_jedynek()
 {
   	int counter=0;
   	for (int i=0; i<20; i++) 
-	  {
-    	for (int j=0; j<20; j++) 
+	{
+    		for (int j=0; j<20; j++) 
 		{
-      		if (tab[i][j] == '1') counter++;
-    	}
+      			if (tab[i][j] == '1') counter++;
+    		}
   	}
   	return counter;
 }
@@ -49,9 +50,9 @@ int main()
   	int rewersCounter = 0, max_jedynek = 0;
 	for (int n = 0; n < SIZE; n++) 
 	{
-    	zczytaj();
-    	if (rewers()) rewersCounter++;
-    	if (razem_jedynek()>max_jedynek)max_jedynek = razem_jedynek();
+    		zczytaj();
+    		if (rewers()) rewersCounter++;
+    		if (razem_jedynek()>max_jedynek)max_jedynek = razem_jedynek();
 	}
 	cout<<"Liczba rewersow: "<<rewersCounter <<endl;
 	cout<<"Najwieksza liczba pikseli czarnych: "<<max_jedynek<< endl;
